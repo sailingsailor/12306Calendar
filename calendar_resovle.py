adf06104bd45a950062dd70fa55efdf6b608217e
sailingsailor <77794256+sailingsailor@users.noreply.github.com>
2024-05-12 16:58:38 +0800

    Update calendar_resovle.py
    增加提取到站时间和检票口

--- calendar_resovle.py
+++ calendar_resovle.py
@@ -1,83 +1,82 @@
-import poplib
-import datetime
-import os
-import re
-import pytz
-import requests
+import poplib, datetime, os, re, pytz, requests, io, json
 import pandas as pd
+from bs4 import BeautifulSoup
 from datetime import datetime
 
-class CalendarResolve:
+class CalendarResovle:
     def generate_calendar_model(self, mail):
         order_info = mail['order_info']
-        train_from_to = re.search(r'\uFF0C([\u4e00-\u9fa5]{2,10}-[\u4e00-\u9fa5]{2,10})\uFF0C', order_info).group(1)
-        # 提取发站和到站
-        pattern_stations = r"([\u4e00-\u9fa5]+站)-([\u4e00-\u9fa5]+站)"
-        stations = re.search(pattern_stations, order_info)
-        start_station = stations.group(1)
-        end_station = stations.group(2)
-        train_number = self.__train_number_text(order_info)
-        train_seat_number = self.__train_seat_number(order_info)
-        train_date_text = self.__train_date_text(order_info)
-        train_time_text = self.__train_time_text(train_date_text)
-        calendar_start = self.__train_datetime(train_date_text)
-        calendar_title = train_from_to.replace('站', '') + ' ' + train_time_text + ' ' + train_number + ' ' + train_seat_number
-        calendar_description = mail['order_id'] + '，' + self.__order_type_text(mail['order_type']) + '，' + order_info
+        begin_time = ''
+        time_pattern = r"\d{4}年\d{2}月\d{2}日\d{2}:\d{2}"
+        time_match = re.search(time_pattern, order_info)
+        if time_match:
+            time = time_match.group()
+            # 解析为 datetime 对象
+            begin_time = datetime.strptime(time, '%Y年%m月%d日%H:%M')
+            print("时间:", begin_time)
+
+        # 提取发站到站，连字符（-）两边的字符串
+        station = ''
+        station_pattern = r"，([^，]+)-([^，]+)，"
+        station_match = re.search(station_pattern, order_info)
+        if station_match:
+            start_station = station_match.group(1).replace("站","")
+            end_station = station_match.group(2).replace("站","")
+            station = start_station + "-" + end_station
+            print("行程:", station)
+            print("发站:", start_station)
+            print("到站:", end_station)
+
+        # 提取车次
+        train = ''
+        train_pattern = r"[A-Z]\d+次"
+        train_match = re.search(train_pattern, order_info)
+        if train_match:
+            train = train_match.group().replace("次","")
+            print("车次:", train)
+
+        # 提取座位号
+        seat = ''
+        seat_pattern = r"\d+车\d+[A-Z]号"
+        seat_match = re.search(seat_pattern, order_info)
+        if seat_match:
+            seat = seat_match.group()
+            print("座位号:", seat)
+
+        # 提取检票口
+        check_in = ''
+        check_in_pattern =  r"检票口(\w+)"
+        check_in_match = re.search(check_in_pattern, order_info)
+        if check_in_match:
+            check_in = check_in_match.group()
+            print("检票口:", check_in.replace("检票口",""))
+        else:
+            print("未找到检票口信息。")
+
         # Function to extract arrival time
-        url = f"http://touch.qunar.com/h5/train/trainStaChoose?trainNum={train_number}"
+        # Extract arrival time from the URL
+        url = f"http://touch.qunar.com/h5/train/trainStaChoose?trainNum={train}"
         content = requests.get(url).text
+        # Wrap the HTML content in a StringIO object
+        content_io = io.StringIO(content)
         # Use pandas to parse the HTML table
-        df = pd.read_html(content)[0]
+        df = pd.read_html(content_io)[0]
         # Filter the DataFrame to get the desired time
         filtered_df = df[df["车站"].str.contains(end_station)]
+        #print(filtered_df)
         if not filtered_df.empty:
             arrival_time = filtered_df["到达时间"].values[0]
-            calendar_end = datetime.strptime(arrival_time, '%Y年%m月%d日%H:%M')
-            print("到站:", end_station, "时间:", arrival_time)
+            #print(arrival_time)
+            end_time = begin_time.replace(hour=int(arrival_time[:2]), minute=int(arrival_time[3:]))
+            print("到达时间:", end_time)
         else:
             print("Time not found.")
-
+        
+        calendar_start = begin_time
+        calendar_end = end_time
+        calendar_title = station + ' ' + train + '次，' + seat
+        calendar_description = mail['order_id'] + '，' + self.__order_type_text(mail['order_type']) + '，' + order_info
         return calendar_title, calendar_start, calendar_end, calendar_description
-
-    def __platform_text(self, order_info_str):
-        result = re.split(',|，|。', order_info_str)
-        for text in result:
-            if "站" in text:
-                return text
-        return ''
-
-    def __train_number_text(self, order_info_str):
-        result = re.split(',|，|。', order_info_str)
-        for text in result:
-            if "次列车" in text:
-                train_number = re.findall(r"[a-zA-Z0-9]+", text)
-                return train_number[0]
-        return ''
-
-    def __train_seat_number(self, order_info_str):
-        result = re.split(',|，|。', order_info_str)
-        for text in result:
-            if "车" in text and "号" in text:
-                return text
-        return ''
-
-    def __train_date_text(self, order_info_str):
-        time_pattern = re.compile(r'\d+年\d+月\d+日\d+:\d+')
-        result = time_pattern.findall(order_info_str)
-        if len(result) == 0:
-            return ''
-        return result[0]
-
-    def __train_time_text(self, date_str):
-        result = re.findall(r"\d+:\d+", date_str)
-        if len(result) == 0:
-            return ''
-        return result[0]
-
-    def __train_datetime(self, date_str) -> datetime:
-        dt_cn = datetime.datetime.strptime(date_str, '%Y年%m月%d日%H:%M')
-        dt_utc = dt_cn.astimezone(tz=pytz.timezone("UTC"))
-        return dt_utc
 
     def __order_type_text(self, type):
         if "insert" == type:
